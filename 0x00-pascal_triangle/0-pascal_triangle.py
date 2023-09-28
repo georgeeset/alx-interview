b83968a41ffa356ed44962a85f2edb55462d84fc
georgeeset <georgeperfect4u@gmail.com>
2023-09-28 21:13:16 +0100

    pascal triangle as easy as pie

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -4,4 +4,16 @@ the Pascal's triangle of n
 """
 
 def pascal_triangle(n):
-    pass
+    big_list = []
+    for i in range(n):
+        small_list = []
+
+        for j in range(i+1):
+            if i==0 or j==0 or i==j or i==1:
+                small_list.append(1)
+            elif i>1 and i>j:
+                small_list.append(big_list[i-1][j-1] + big_list[i-1][j])
+        
+        big_list.append(small_list)
+
+    return big_list
