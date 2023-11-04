5494639166786f26c2aca17514779697d28ecf34
georgeeset <georgeperfect4u@gmail.com>
2023-11-04 10:56:25 +0100

    nqueen sorted with ai and i

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,46 +1,53 @@
 #!/usr/bin/python3
-""" module docs """
-from sys import argv, exit
+"""Solving N Queens Problem using backtracking"""
+import sys
+from typing import List
+
+
+def printSolution(board: List[List], n: int) -> None:
+    """Print allocated positions to the queen"""
+    positions = []
+
+    for row in range(n):
+        for column in range(n):
+            if column == board[row]:
+                positions.append([row, column])
+    print(positions)
+
+
+def is_safe(board: List[List], r: int, c: int, row: int) -> bool:
+    """Checks if the position is safe"""
+    return board[r] in (c, c - r + row, r - row + c)
+
+
+def recursive_fill(board: List, row, n) -> None:
+    """recursively select locations"""
+    if row == n:
+        printSolution(board, n)
+
+    else:
+        for c in range(n):
+            allowed = True
+            for r in range(row):
+                if is_safe(board, r, c, row):
+                    allowed = False
+            if allowed:
+                board[row] = c
+                recursive_fill(board, row + 1, n)
+
 
 if __name__ == "__main__":
-
-    if len(argv) != 2:
+    if len(sys.argv) != 2:
         print("Usage: nqueens N")
-        exit(1)
-
-    queens = argv[1]
-
-    try:
-        queens = int(queens)
-    except ValueError:
+        sys.exit(1)
+    if sys.argv[1].isdigit() is False:
         print("N must be a number")
-        exit(1)
-
-    if queens < 4:
+        sys.exit(1)
+    if int(sys.argv[1]) < 4:
         print("N must be at least 4")
-        exit(1)
+        sys.exit(1)
 
-    solution = []
+    n = int(sys.argv[1])
 
-    def nqueens(row, queens, solution):
-        """ method docs """
-        if (row == queens):
-            print(solution)
-        else:
-            for col in range(queens):
-                pos = [row, col]
-                if validposition(solution, pos):
-                    solution.append(pos)
-                    nqueens(row + 1, queens, solution)
-                    solution.remove(pos)
-
-    def validposition(solution, position):
-        """ method docs """
-        for queen in solution:
-            if queen[1] == position[1] or \
-                (queen[0] - queen[1]) == (position[0] - position[1]) or \
-                    (queen[0] + queen[1]) == (position[0] + position[1]):
-                return False
-        return True
-
-    nqueens(0, queens, solution)
+    board = [0 * n for i in range(n)]
+    recursive_fill(board, 0, n)
