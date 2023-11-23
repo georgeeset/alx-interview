f898eae92457a9788df1647177cd9f16d73bb951
georgeeset <georgeperfect4u@gmail.com>
2023-11-23 08:28:37 +0100

    using classic bottom up dynamic programming

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -7,19 +7,15 @@ def makeChange(coins, total):
     """  determine the fewest number of coins
     needed to meet a given amount total
     """
-    if total <= 0:
+    temp_value = 0
+    coins.sort(reverse=True)
+
+    if total < 0:
         return 0
-    memory = total
-    i = 0
-    coin_count = 0
-    sorted_coins = sorted(coins, reverse=True)
-    n = len(coins)
-    while memory > 0:
-        if i >= n:
-            return -1
-        if memory - sorted_coins[i] >= 0:
-            memory -= sorted_coins[i]
-            coin_count += 1
-        else:
-            coin_idx += 1
-    return coin_count
+
+    for coin in coins:
+        if total % coin <= total:
+            temp_value += total // coin
+            total = total % coin
+
+    return temp_value if total == 0 else -1
